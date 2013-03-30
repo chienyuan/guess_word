445f222025ed39b8d65864140bff5ae142ba9cdf
Eric Feng <chienyuan.feng@gmail.com>
2013-03-30 09:50:35 -0500

    fix aspell not recognize germany but GERMANY

--- guess.rb
+++ guess.rb
@@ -4,7 +4,7 @@ require 'ffi/aspell'
 
 
 if ARGV.length < 2
-  puts "Usage: guess num abcde..."
+  puts "Usage: guess.rb pattern ABCDE... (ex. guess.rb ****Y GERMARY) "
   exit
 end
 
@@ -14,24 +14,49 @@ speller = FFI::Aspell::Speller.new('en_US')
 #length = 4
 
 args = ARGV.clone
-length =  args.shift.to_i
-letters = args.shift.split('').to_a
-
+pattern  =  args.shift.upcase.split('').to_a
+length = 0
+pattern_flag = false
+pattern.each do |x|
+  if x == '.'
+    length = length + 1
+  else
+    pattern_flag = true
+  end
+end
+letters = args.shift.upcase.split('').to_a
 size = letters.length
 total = size.downto(size - length + 1).inject(1) { |r,e| r * e }
-puts letters.join
-puts "size=#{size}"
-puts "total = #{total}"
-puts "expect time: #{total * 0.007} seconds"
+
+puts "Input:"
+puts "Pattern = #{pattern}"
+puts "Length  = #{length}"
+puts "Letters = #{letters.join}"
+puts "Size    = #{size}"
+puts "Total   = #{total}"
+puts "Expect time: #{total * 0.007} seconds"
+
+puts
+puts "Output:"
 words = []
-letters.combination(length).entries.each do |x|
-  x.permutation.entries.each do |y|
-    word = y.join
+letters.permutation(length).entries.each do |y|
+    if pattern_flag
+      aword = []
+      pattern.each do |x|
+        if x == '.'
+          aword.push y.shift
+        else
+          aword.push x
+        end
+      end
+      word = aword.join
+    else
+      word = y.join
+    end
     if not words.include?(word) and speller.correct?(word)
       puts word
       words << word 
     end
-  end
 end
 
-puts "total: #{words.length}/#{total}"
+puts "Total: #{words.length}/#{total}"
